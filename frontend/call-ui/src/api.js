a2b2ea07cc9f6a84429dab268a33e419b9db70f8
Nafishapanacea <nafisha.begum@radpretation.ai>
2026-07-06 12:19:38 +0530

    Omen deployment

--- frontend/call-ui/src/api.js
+++ frontend/call-ui/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = "https://omen.radpretation.ai/gemini-twilio-telephony"
 
 export const triggerCall = async (phoneNumber) => {
     const response = await axios.post(
--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import uvicorn
 uvicorn.run(
     "app.main:app",
     host="0.0.0.0",
-    port=8000,
+    port=8018,
     reload=False
 )
